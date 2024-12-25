95bca4a7817abe2e7755229ddfa9c67279cc05b9
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-25 11:57:36 +0500

    Add logging

--- src/clouds/yandex_cloud.py
+++ src/clouds/yandex_cloud.py
@@ -1,3 +1,4 @@
+from logging import getLogger
 import os.path
 from urllib.request import pathname2url
 from typing import Dict
@@ -8,6 +9,8 @@ from requests import Response
 
 from .cloud import Cloud
 
+logger = getLogger("main.yandex_cloud")
+
 
 class YandexCloud(Cloud):
 
@@ -16,31 +19,49 @@ class YandexCloud(Cloud):
         self.base_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         self.headers = {"Authorization": f"OAuth {token}"}
 
-    def load(self, path):
+    def load(self, path, overwrite: bool = False):
+        logger.info("Start uploading a file to yandex disk")
+
         filename = os.path.basename(path)
+        logger.debug("Filename is %s", filename)
         remote_path = os.path.join(self.remote_dir_path, filename)
-        url = "?".join((self.base_url, f"path={pathname2url(remote_path)}"))
-        print(url)
+        logger.debug("Remote path is %s", remote_path)
+        remote_path = pathname2url(remote_path)
+        args: str = "path={remote_path}&overwrite={overwrite}".format(
+            remote_path=remote_path,
+            overwrite="true" if overwrite else "false",
+        )
+
+        url = "?".join((self.base_url, args))
+        logger.debug("Getting href to uploading file...")
         response: Response = requests.get(
             url,
             headers=self.headers
         )
 
         data_dict = response.json()
-        print(data_dict)
         if response.status_code != 200:
-            raise ValueError(f"First request failed..., status code: {response.status_code}", )
+            logger.error("Status code is %d (must be 200)."
+                         " Response body:\n%s", response.status_code, str(data_dict))
+            raise ValueError("Status code is not 200")
 
+        logger.debug("Uploading file...")
         response = requests.put(
             data_dict["href"],
             headers=self.headers,
             data=open(path, "rb")
         )
-        if not 200 <= response.status_code < 300:
-            raise ValueError(f"Second request failed..., status code {response.status_code}")
+        if response.status_code != 201 and response.status_code != 202:
+            logger.error(
+                "The download link was received, but the download failed.\n"
+                "Status code - %d (must be 201 or 202). Response body:\n%s",
+                response.status_code, str(response.json())
+            )
+            raise ValueError("The download link was received, but the download failed.")
+        logger.debug("Done")
 
     def reload(self, path):
-        pass
+        self.load(path, overwrite=True)
 
     def delete(self, filename):
         pass
