5fbf5a1795dcc61a631bc2e9d7b496bcefed8dd5
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-25 14:51:42 +0500

    Delete unnecessary items

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
+import json
+import logging.config
+from logging import getLogger
+from typing import List
+from urllib.parse import unquote
+
 from utils.files import search_files
 from config.env_config import Config
+from config.log_config import log_config
 from clouds.yandex_cloud import YandexCloud
 from clouds.cloud import Cloud
 
+logging.config.dictConfig(log_config)
+logger = getLogger("main")
 
-if __name__ == "__main__":
-    config = Config()
-    print(config)
-    cloud: YandexCloud = YandexCloud(config.token, config.remote_dir_path)
-    print(cloud.headers)
-    for file_path in search_files(config.target, depth=0):
-        print(file_path)
-        cloud.load(file_path)
-        print("Done!")
+
+def main():
+    logger.debug("Starting app...")
+    config: Config = Config()
