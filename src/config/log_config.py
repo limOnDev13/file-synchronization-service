e16094413de64f870d3e0c9271fafa375c3d0f56
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-25 12:12:58 +0500

    ADd config for logging

---
+++ src/config/log_config.py
@@ -0,0 +1,39 @@
+from typing import Dict, Any
+
+
+log_config: Dict[str, Any] = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "file": {
+            "format": "[%(asctime)s] %(message)s",
+        },
+        "console": {
+            "format": "[%(levelname)s] [%[asctime]s] %(module)s.%(funcName)s - %(message)s",
+        },
+    },
+    "handlers": {
+        "file": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "level": "INFO",
+            "formatter": "file",
+            "filename": "logfile.log",
+            "mode": "a",
+            "backupCount": 3,
+            "when": "d",
+            "interval": 10,
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "console",
+        },
+    },
+    "loggers": {
+        "main": {
+            "level": "DEBUG",
+            "handlers": ["file", "console"],
+            "propagate": False,
+        },
+    },
+}
