1bfc3413ae10542f4c4b663582e21ccf7e353e97
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-25 14:30:24 +0500

    Add method get_info; fix method _query_params

--- src/clouds/yandex_cloud.py
+++ src/clouds/yandex_cloud.py
@@ -1,7 +1,7 @@
 from logging import getLogger
 import os.path
 from urllib.request import pathname2url
-from typing import Dict
+from typing import Dict, Optional, List, Any
 import json
 
 import requests
@@ -19,14 +19,24 @@ class YandexCloud(Cloud):
         self.base_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         self.headers = {"Authorization": f"OAuth {token}"}
 
-    def _query_params(self, path, kwargs: Dict[str, int | str]) -> str:
-        filename = os.path.basename(path)
-        logger.debug("Filename is %s", filename)
-        remote_path = os.path.join(self.remote_dir_path, filename)
+    def _query_params(self, path, is_dir: bool = False, kwargs: Optional[Dict[str, int | str]] = None) -> str:
+        if is_dir:
+            remote_path = self.remote_dir_path
+        else:
+            filename = os.path.basename(path)
+            logger.debug("Filename is %s", filename)
+            remote_path = os.path.join(self.remote_dir_path, filename)
+
         logger.debug("Remote path is %s", remote_path)
         remote_path = pathname2url(remote_path)
 
-        return "&".join((f"path={remote_path}", *(f"{key}={value}" for key, value in kwargs.items())))
+        if kwargs is not None:
+            return "&".join((
+                f"path={remote_path}",
+                *(f"{key}={value}" for key, value in kwargs.items())
+            ))
+        else:
+            return f"path={remote_path}"
 
     def load(self, path, overwrite: bool = False):
         logger.info("Start uploading a file to yandex disk")
@@ -82,5 +92,49 @@ class YandexCloud(Cloud):
             raise ValueError("Status code is not 204")
         logger.debug("Done")
 
-    def get_info(self):
-        pass
+    def get_info(self) -> List[Dict[str, Any]]:
+        logger.info("Start getting info about remote dir.")
+        items: List[Dict[str, Any]] = list()
+        limit: int = 20
+        offset: int = 0
+
+        url = "?".join((
+            "https://cloud-api.yandex.net/v1/disk/resources",
+            self._query_params(self.remote_dir_path, is_dir=True, kwargs={"limit": limit, "offset": offset}),
+        ))
+        response: Response = requests.get(
+            url,
+            headers=self.headers
+        )
+        if response.status_code != 200:
+            logger.error("Status code is %d (must be 200)."
+                         " Response body:\n%s", response.status_code, str(response.json()))
+            raise ValueError("Status code is not 200")
+
+        data_json = response.json()["_embedded"]
+        items.extend(data_json["items"])
+
+        #
+        if data_json["total"] > limit:
+            logger.debug("There were more files than the limit=%s, making a repeat request...", limit)
+            offset = limit
+            limit = data_json["total"]
+
+            url = "?".join((
+                "https://cloud-api.yandex.net/v1/disk/resources",
+                self._query_params(self.remote_dir_path, is_dir=True, kwargs={"limit": limit, "offset": offset}),
+            ))
+            response: Response = requests.get(
+                url,
+                headers=self.headers
+            )
+            if response.status_code != 200:
+                logger.error("Status code is %d (must be 200)."
+                             " Response body:\n%s", response.status_code, str(response.json()))
+                raise ValueError("Status code is not 200")
+
+            data_json = response.json()["_embedded"]
+            items.extend(data_json["items"])
+
+        logger.debug("Done")
+        return items
