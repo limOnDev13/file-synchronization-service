f7c269794c66c59642a9137a970688d753053f86
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-25 21:27:55 +0500

    Fix func main - now the function adjusts and starts synchronization in an endless loop

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 """A module for configuring and launching the application."""
 
 import logging.config
+import time
 from logging import getLogger
 
+from clouds.cloud import Cloud
+from clouds.yandex_cloud import YandexCloud
+from config.env_config import Config
 from config.log_config import log_config
+from src.logic.syncronization import synchronize
 
 logging.config.dictConfig(log_config)
 logger = getLogger("main")
@@ -12,3 +17,16 @@ logger = getLogger("main")
 def main():
     """Configure and launch the app."""
     logger.debug("Starting app...")
+    config: Config = Config()
+    cloud: Cloud = YandexCloud(
+        token=config.token,
+        remote_dir_path=config.remote_dir_path,
+    )
+
+    while True:
+        synchronize(cloud=cloud, init_dir=config.target)
+        time.sleep(config.delay)
+
+
+if __name__ == "__main__":
+    main()
