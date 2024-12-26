7945e1245f3a90b26c17ca8cae31b6156e54e27a
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-26 10:20:29 +0500

    Create class Delay for config delay; add method __init__ for class Config;

--- src/config/env_config.py
+++ src/config/env_config.py
@@ -9,10 +9,31 @@ load_dotenv()
 
 
 @dataclass
+class Delay(object):
+    """Config for delay."""
+
+    days: int = int(os.getenv("DAYS", 0))
+    hours: int = int(os.getenv("HOURS", 0))
+    minutes: int = int(os.getenv("MINUTES", 0))
+    seconds: int = int(os.getenv("SECONDS", 0))
+
+    def __init__(self):
+        if (
+            self.days == 0
+            and self.hours == 0
+            and self.minutes == 0
+            and self.seconds == 0
+        ):
+            self.minutes = 1
+
+
+@dataclass
 class Config(object):
     """Config class."""
 
     token: str = os.getenv("TOKEN", "")
     target: str = os.getenv("TARGET", ".")
     remote_dir_path: str = os.getenv("REMOTE_DIR_PATH", "")
-    delay: int = int(os.getenv("DELAY", 60))
+
+    def __init__(self):
+        self.delay = Delay()
